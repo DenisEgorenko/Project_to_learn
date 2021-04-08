e04379691f27f9c65382a3e0af5aa2044cde4979
Denis <denisegorenko1@gmail.com>
2021-04-09 00:11:57 +0300

    Fifth Commit / Social Network

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './App.css';
 
-import Header from "./Header";
-import Nav from "./Nav_bar";
-import Profile from "./Profile";
+import Header from "./Components/Header";
+import Nav from "./Components/Nav_bar";
+import Profile from "./Components/Profile";
 
 
 function App() {
